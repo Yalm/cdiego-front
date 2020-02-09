aecc191e76c4ba204787ad5aed2f36c3434ae77e
Yalm <mcondori@dtransforma.com>
2020-02-08 23:58:15 -0500

    culqi

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
     production: true,
     img_default: "https://i.stack.imgur.com/l60Hf.png",
-    cuqli: {
-        public_key: "pk_test_R9BjlKbSaIrcXgMP"
+    culqi: {
+        public_key: 'pk_test_R9BjlKbSaIrcXgMP'
     },
     apiUrl: 'https://cdiego-back.herokuapp.com'
 };
